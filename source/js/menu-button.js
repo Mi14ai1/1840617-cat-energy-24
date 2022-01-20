1a3cbe74b462105aca4b729310ffc496aef4362c
Mikhail Bootko <m14ai1@yandex.ru>
2022-01-20 22:52:38 +0300

    many-fixes

--- source/js/menu-button.js
+++ source/js/menu-button.js
@@ -1,12 +1,13 @@
-var nav = document.querySelector('.navigation');
-var navToggle = document.querySelector('.navigation__button');
-nav.classList.remove('navigation--nojs');
-nav.addEventListener('click', function() {
-  if (nav.classList.contains('navigation--closed')) {
-    nav.classList.remove('navigation--closed');
-    nav.classList.add('navigation--opened');
+var nav = document.querySelector(".navigation");
+var navToggle = document.querySelector(".navigation__button");
+nav.classList.add("navigation--closed");
+nav.classList.remove("navigation--nojs");
+nav.addEventListener("click", function() {
+  if (nav.classList.contains("navigation--closed")) {
+    nav.classList.remove("navigation--closed");
+    nav.classList.add("navigation--opened");
   } else{
-    nav.classList.add('navigation--closed');
-    nav.classList.remove('navigation--opened');
+    nav.classList.add("navigation--closed");
+    nav.classList.remove("navigation--opened");
   }
 });
